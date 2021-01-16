94050a2a6b27ee4d1d26e8f5bf15ec52f68cbfc1
niche-dev <info@niche.com.hk>
2021-01-17 00:05:13 +0800

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath app001 app001.wsgi
--- app001/wsgi.py
+++ app001/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for app001 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
+# mysite/mysite/wsgi.py
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app001.settings')
+from dj_static import Cling
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app001.settings")
+
+application = Cling(get_wsgi_application())
